1cc44b28850fdab260ba33549a018070bf8ee1bc
enmanuelCastillo <enmanuel.castillo@hotmail.com>
2020-06-17 15:41:18 -0400

    agregando archivos estaticos

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'django_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -131,3 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
