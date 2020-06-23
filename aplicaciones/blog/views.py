f38064d99b33c15500a662c0d82546d45828200c
enmanuelCastillo <enmanuel.castillo@hotmail.com>
2020-06-22 22:01:56 -0400

    pagination33

--- aplicaciones/blog/views.py
+++ aplicaciones/blog/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from django.db.models import Q
 from .models import Post, Categoria
-
+from django.core.paginator import Paginator
 def home(request):
     queryset = request.GET.get('buscar')
-    print(queryset)
     posts = Post.objects.filter(estado = True)
     if queryset:
         posts = Post.objects.filter(
         Q(titulo__icontains = queryset) |
         Q(descripcion__icontains = queryset)
         ).distinct()
+    paginator = Paginator(posts,3)
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
     return render(request, 'index.html', {'posts':posts})
 
 
@@ -29,6 +31,9 @@ def generales(request):
             estado = True,
             categoria = Categoria.objects.get(nombre__iexact = 'Generales')
         )
+    paginator = Paginator(posts,3)
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
     return render(request, 'generales.html', {'posts':posts})
 
 
