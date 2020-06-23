b7c56ce12a4721debfeb0a461588ef629357a7b9
enmanuelCastillo <enmanuel.castillo@hotmail.com>
2020-06-23 15:01:11 -0400

    credenciales HEROKU

--- django_blog/settings/production.py
+++ django_blog/settings/production.py
@@ -10,8 +10,12 @@ ALLOWED_HOSTS = ['djangoblogcastillo.herokuapp.com']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd74gauu4qds9in',
+        'USER': 'vvdiubraqnkqyq',
+        'PASSWORD': '522a29ff1ee405f73134d913a2b5f2306f9bb986bfe350ff93b3422cad7a591a',
+        'HOST': 'ec2-52-20-248-222.compute-1.amazonaws.com',
+        'PORT': 5432
     }
 }
 
